7fec8aaca945a6afdef086a25164818d6f3cb482
dmytro <dmytroivantsiv1@gmail.com>
2024-12-07 04:35:42 +0200

    fix: migrate supabase from dev to client

--- src/config/supabase.ts
+++ src/config/supabase.ts
@@ -2,8 +2,8 @@ import { createClient } from '@supabase/supabase-js'
 import { Database } from '../Types/supabase'
 
 
-const supabaseUrl = 'https://kvllqttkeseeojuwvcnw.supabase.co'
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt2bGxxdHRrZXNlZW9qdXd2Y253Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI2MTQ3NTgsImV4cCI6MjAzODE5MDc1OH0.faTCBEOh2e7gFvbr6DTALBQT42q-P331Cw1yledrS-0"
+const supabaseUrl = 'https://fuxmqpbffwqxdyvwrbgd.supabase.co'
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ1eG1xcGJmZndxeGR5dndyYmdkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMxMDc1MTEsImV4cCI6MjA0ODY4MzUxMX0.DVwnySnUxuCEAyGwGJ_u-XLKuOER6d8uO1EQWR5ZQEs"
 
 
 export const supabaseClient = createClient<Database>(supabaseUrl, supabaseKey)
